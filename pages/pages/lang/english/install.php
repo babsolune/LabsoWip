ec34478e22070bfb6ef8795ad85f84b767b0f25e
Labsoweb\babsolune <babsolune@phpboost.com>
2020-06-28 03:11:03 +0200

    [UPD] pages

---
+++ pages/pages/lang/english/install.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2020 06 15
+ * @since       PHPBoost 5.2 - 2020 06 15
+*/
+
+####################################################
+#                       English                    #
+####################################################
+
+$lang['default.cat.name'] = 'Test category';
+$lang['default.cat.description'] = 'Test files';
+$lang['default.page.name'] = 'Pages module logo';
+$lang['default.page.content'] = 'PHPBoost pages module logo';
+?>
--- staff/README.md
+++ staff/README.md
@@ -1,3 +1,3 @@
 # Module Staff for phpboost 6.0
-The module has to be converted for the phpboost 6.0 version
+The module has to be converted for the phpboost 6.0 version  
 [download](https://downgit.github.io/#/home?url=https://github.com/babsolune/LabsoWip/tree/master/staff)
